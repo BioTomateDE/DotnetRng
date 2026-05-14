692214a6b73abbdea5bedf8326ceb78cbe8fd09d
BioTomateDE <biotomatede@proton.me>
2026-05-14 19:28:40 +0200

    1.0.4: Even more methods i didn't know existed, extensive testing

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# DotnetRNG
+# dotnet-rng
 
 A Rust implementation of .NET's Random algorithm based on Knuth's subtractive method.
 
 This crate is:
 
-* extremely lightweight (no dependencies, low amount of code)
-* compatible with embedded systems (`no_std` and `no_alloc`)
-* entirely usable in constant evaluation (all functions are marked as `const`)
-* platform-independent (no usage of `usize` or pointers in `struct`s)
 * tested to match exactly with .NET generated values
+* extremely lightweight (no dependencies, low amount of code)
+* entirely usable in constant evaluation (all functions are marked as `const`)
+* compatible with embedded systems (`no_std` and `no_alloc`)
+* platform-independent (no usage of `usize` or pointers in `struct`s)
 
 ## Usage
 
@@ -18,25 +18,31 @@ use dotnet_rng::DotnetRng;
 // Create a new RNG instance with a given seed
 let mut rng = DotnetRng::new(1337);
 
-// Generate integer between [-2147483648, 2147483648)
-let num: i32 = rng.next();
+// Generate integer between [0, 2147483647)
+let int: i32 = rng.next();
 
 // Generate integer between [100, 200)
-let num: i32 = rng.next_ranged(100, 200);
+let int: i32 = rng.next_range(100, 200);
 
-// Advance internal state (same as discarding rng.next() return value)
-rng.skip();
+// Generate long between [0, 9223372036854775807)
+let long: i64 = rng.next_i64();
 
-// Generate number between [0, 1)
-let num: f64 = rng.next_f64();
+// Generate long between [-69, 420)
+let long: i64 = rng.next_i64_range(-69, 420);
+
+// Generate double-precision float between [0, 1)
+let double: f64 = rng.next_f64();
 
 // Generate 64 random bytes
 let bytes: [u8; 64] = rng.next_bytes();
 
 // Fill existing byte buffer
-let mut buffer = [0u8; 100];
+let mut buffer = [0u8; 187];
 rng.fill_bytes(&mut buffer);
 println!("Bytes: {buffer:?}");
+
+// Advance internal state (same as discarding rng.next() return value)
+rng.skip();
 
 // RNG is deterministic
 let mut new_rng = rng.clone();
@@ -56,7 +62,9 @@ would get for the same seed in .NET, then this crate is for you.
 ## Reference
 
 The RNG algorithm is taken from the C# System library:
-<https://github.com/microsoft/referencesource/blob/ec9fa9ae770d522a5b5f0607898044b7478574a3/mscorlib/system/random.cs>
+[Archive file](https://github.com/microsoft/referencesource/blob/main/mscorlib/system/random.cs)
+|
+[Current file](https://github.com/dotnet/dotnet/blob/main/src/runtime/src/libraries/System.Private.CoreLib/src/System/Random.CompatImpl.cs).
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
 #![doc = include_str!("../README.md")]
 #![no_std]
 #![forbid(unsafe_code)]
-#![warn(clippy::pedantic, clippy::nursery, clippy::cargo, missing_docs)]
-#![allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-
-#[cfg(test)]
-mod tests;
+#![warn(clippy::nursery, clippy::cargo, missing_docs)]
 
 /// MSEED is based on an approximation of the golden ratio (phi):
 /// MSEED ≈ φ * 10^8
 const MSEED: i32 = 161_803_398;
 
-/// The random number generator used by **.NET**.
+/// The random number generator used by **.NET** for seeded RNG.
 ///
 /// All instances of this struct initialized with
 /// the same seed will always produce the same values.
@@ -88,19 +84,16 @@ impl DotnetRng {
         }
     }
 
-    /// Generates a new number and throws it away.
+    /// Generates the next 32-bit signed integer.
     ///
-    /// This is useful if you want to advance the RNG's internal
-    /// state without actually using the generated number.
-    #[inline]
-    pub const fn skip(&mut self) {
-        let _ = self.next();
-    }
-
-    /// Generates the next 32-bit signed integer
-    /// and advances the state of the RNG.
+    /// The generated number is never negative and never exactly [`i32::MAX`].
     ///
-    /// **Range**: [`i32::MIN`] .. [`i32::MAX`]
+    /// # Related
+    /// This function is equivalent to the C# method overload `Next()`.
+    ///
+    /// # Notes
+    /// This function is the base for all other functions.
+    /// Calling `.next()` one time means advancing the internal state of the RNG once.
     #[doc(alias = "next_i32")]
     #[doc(alias = "next_int")]
     #[must_use = "if you intend to only advance the internal rng state, use `.skip()`"]
@@ -134,15 +127,23 @@ impl DotnetRng {
 
     /// Generates the next signed 32-bit integer within the given range.
     ///
-    /// **Range**: `min` .. `max`
+    /// The generated number is greater or equal to `min` and less than `max`.
+    /// Exception: If `min == max`, then `min` is returned.
     ///
-    /// If the range is [`i32::MAX`] or larger, a slightly different algorithm
+    /// # Related
+    /// This function is equivalent to the C# method overload `Next(int minValue, int maxValue)`.
+    /// The overload `Next(int maxValue)` can be simulated using `next_range(0, max)`.
+    ///
+    /// # Notes
+    /// If the range is larger than [`i32::MAX`], a slightly different algorithm
     /// is used which internally calls `.next()` twice instead of once.
     ///
     /// # Panics
     /// This function will panic if `min > max`.
-    #[must_use = "if you intend to only advance the internal rng state, use `.skip()`"]
-    pub const fn next_ranged(&mut self, min: i32, max: i32) -> i32 {
+    #[doc(alias = "next_ranged")]
+    #[doc(alias = "next_between")]
+    #[must_use]
+    pub const fn next_range(&mut self, min: i32, max: i32) -> i32 {
         assert!(min <= max, "minimum is greater than maximum");
 
         if let Some(range) = max.checked_sub(min) {
@@ -156,20 +157,113 @@ impl DotnetRng {
         }
         let mut num: f64 = sample as f64;
         num += (i32::MAX - 1) as f64;
-        num /= (2 * (i32::MAX) as u32) as f64 - 1.0;
+        num /= (2 * (i32::MAX) as u64) as f64 - 1.0;
 
         let range: f64 = (max as f64) - (min as f64);
-        (num * range) as i32 + min
+        ((num * range) as i64 + min as i64) as i32
+    }
+
+    const fn next_u64(&mut self) -> u64 {
+        let low = self.next_range(0, 1 << 22) as u64;
+        let mid = self.next_range(0, 1 << 22) as u64;
+        let high = self.next_range(0, 1 << 20) as u64;
+        low | (mid << 22) | (high << 44)
+    }
+
+    /// Generates the next 64-bit signed integer.
+    ///
+    /// The generated number is never negative and never exactly [`i64::MAX`].
+    ///
+    /// # Related
+    /// This function is equivalent to the C# method overload `NextInt64()`.
+    ///
+    /// # Notes
+    /// In most cases, this will call `self.next()` thrice.
+    /// However, it is possible that the generated number is exactly
+    /// [`i64::MAX`], which is rejected and causes another iteration.
+    #[doc(alias = "next_int64")]
+    #[doc(alias = "next_int_64")]
+    #[must_use]
+    pub const fn next_i64(&mut self) -> i64 {
+        loop {
+            let num = (self.next_u64() >> 1) as i64;
+            if num != i64::MAX {
+                return num;
+            }
+
+            // The chances of this happening is 1 in 9223372036854775807.
+            cold();
+        }
+    }
+
+    /// Generates the next 64-bit signed integer within the given range.
+    ///
+    /// The generated number is greater or equal to `min` and less than `max`.
+    /// Exception: If `min == max`, then `min` is returned.
+    ///
+    /// # Related
+    /// This function is equivalent to the C# method overload `NextInt64(long minValue, long maxValue)`.
+    /// The overload `NextInt64(int maxValue)` can be simulated using `next_i64_range(0, max)`.
+    ///
+    /// # Panics
+    /// This function will panic if `min > max`.
+    #[doc(alias = "next_range_i64")]
+    #[must_use]
+    pub const fn next_i64_range(&mut self, min: i64, max: i64) -> i64 {
+        assert!(min <= max, "minimum is greater than maximum");
+        let range = max.wrapping_sub(min) as u64;
+        if range <= 1 {
+            return min;
+        }
+
+        let bits = range.ilog2() + !range.is_power_of_two() as u32;
+        loop {
+            let num = self.next_u64();
+            let shift = u64::BITS - bits;
+            let result = num >> shift;
+            if result < range {
+                return (result as i64).wrapping_add(min);
+            }
+        }
     }
 
     /// Generates the next double-precision floating point number.
     ///
-    /// **Range**: 0 .. 1
+    /// The generated number is greater or equal to 0 and less than 1.
+    ///
+    /// # Related
+    /// This function is equivalent to the C# method `NextDouble()`.
     #[doc(alias = "next_double")]
     #[inline]
     #[must_use = "if you intend to only advance the internal rng state, use `.skip()`"]
     pub const fn next_f64(&mut self) -> f64 {
         self.next() as f64 * (1.0 / i32::MAX as f64)
+    }
+
+    /// Generates the next single-precision floating point number.
+    ///
+    /// The generated number is greater or equal to 0 and less than 1.
+    ///
+    /// # Related
+    /// This function is equivalent to the C# method `NextSingle()`.
+    ///
+    /// # Notes
+    /// In most cases, this will only call `self.next()` once.
+    /// However, it is possible that the generated double rounds to `1.0`
+    /// when rounded to a single-precision float, which is rejected and causes another iteration.
+    #[doc(alias = "next_single")]
+    #[doc(alias = "next_float")]
+    #[must_use]
+    pub const fn next_f32(&mut self) -> f32 {
+        loop {
+            let num = self.next_f64() as f32;
+            if num <= 1.0 {
+                return num;
+            }
+
+            // Very low chance for the number to round to 1.0
+            cold();
+        }
     }
 
     /// Fills a given buffer with random bytes.
@@ -180,6 +274,9 @@ impl DotnetRng {
     ///
     /// If you have a known array size at compile-time, consider using
     /// [`DotnetRng::next_bytes`] instead.
+    ///
+    /// # Related
+    /// This function is equivalent to the C# method `NextBytes(Byte[])`.
     pub const fn fill_bytes(&mut self, buffer: &mut [u8]) {
         let mut i = 0;
         while i < buffer.len() {
@@ -197,10 +294,61 @@ impl DotnetRng {
     /// If you do not have a known array size at compile-time, consider using
     /// [`DotnetRng::fill_bytes`].
     #[inline]
-    #[must_use = "if you intend to only advance the internal rng state, use `.skip()`"]
+    #[must_use = "if you intend to only advance the internal rng state, use `.skip_n()`"]
     pub const fn next_bytes<const N: usize>(&mut self) -> [u8; N] {
         let mut buffer = [0u8; N];
         self.fill_bytes(&mut buffer);
         buffer
     }
+
+    /// Generates a new number and throws it away.
+    ///
+    /// This is useful if you want to advance the RNG's internal
+    /// state without actually using the generated number.
+    ///
+    /// This function is equivalent to:
+    /// ```
+    /// # let mut rng = dotnet_rng::DotnetRng::new(0);
+    /// let _ = rng.next();
+    /// ```
+    #[inline]
+    pub const fn skip(&mut self) {
+        let _ = self.next();
+    }
+
+    /// Generates `count` numbers and throws them away.
+    ///
+    /// This is useful if you want to advance the RNG's internal
+    /// state without actually using the generated numbers.
+    ///
+    /// This function is equivalent to:
+    /// ```
+    /// # let mut rng = dotnet_rng::DotnetRng::new(0);
+    /// # let count = 8;
+    /// for i in 0..count {
+    ///     let _ = rng.next();
+    /// }
+    /// ```
+    #[inline]
+    pub const fn skip_n(&mut self, count: u32) {
+        let mut i = 0;
+        while i < count {
+            self.skip();
+            i += 1;
+        }
+    }
 }
+
+// Compile-time tests
+const fn _assert() {
+    use core::panic::{RefUnwindSafe, UnwindSafe};
+    const fn require<T: Clone + Send + Sync + Unpin + UnwindSafe + RefUnwindSafe>() {}
+    require::<DotnetRng>();
+}
+
+const _: () = assert!(size_of::<DotnetRng>() == 228);
+
+// This can be replaced by `core::hint::cold_path` when MSRV >= 1.95.0
+#[inline(always)]
+#[cold]
+const fn cold() {}
